70189886d9358e9b443be663c8181416cf72de4b
MoisesKersch <lightspeedoff@gail.com>
2018-10-08 01:33:21 -0300

    Update

--- src/main/java/com/example/websocketdemo/ChatappApplication.java
+++ src/main/java/com/example/websocketdemo/ChatappApplication.java
@@ -1,12 +1,16 @@
 package com.example.websocketdemo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+//@SpringBootApplication
+//public class ChatappApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ChatappApplication.class, args);
+//	}
+//}
 
 @SpringBootApplication
-public class ChatappApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ChatappApplication.class, args);
-	}
+public class ChatappApplication extends SpringBootServletInitializer {
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -23,7 +23,7 @@ function connect(event) {
         usernamePage.classList.add('hidden');
         chatPage.classList.remove('hidden');
 
-        var socket = new SockJS('/ws');
+        var socket = new SockJS('/chatapp/ws');
         
         stompClient = Stomp.over(socket);
 
